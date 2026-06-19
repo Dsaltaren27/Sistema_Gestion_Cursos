ff6fd880a5fbc79696ee2a7c49940bdfa1ff3f08
dsaltaren07 <dsaltaren07@gmail.com>
2026-06-19 13:25:29 -0500

    implementacion de rotacion de logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
 
 
 const logger = winston.createLogger({
@@ -9,16 +10,25 @@ const logger = winston.createLogger({
         winston.format.json()
     ),
 
-    transports: [
-        new winston.transports.File({
-            filename: 'logs/error.log',
-            level: 'error'
-        }),
 
-        new winston.transports.File({
-            filename: 'logs/combined.log'
-        })
-    ]
+  transports: [
+
+    new DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      maxSize: '20m',
+      maxFiles: '14d'
+    }),
+
+    new DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'error',
+      maxSize: '20m',
+      maxFiles: '30d'
+    })
+
+  ]
 });
 
 if(process.env.NODE_ENV !== 'production'){
