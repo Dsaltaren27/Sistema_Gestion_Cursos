1253661369b5ca81f49a18e85bf5344432014030
dsaltaren07 <dsaltaren07@gmail.com>
2026-06-22 15:20:49 -0500

    implementacion de openapi

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { login, register } = require('../controller/auth.controller');
 const { validateLogin, validateRegister } = require('../middleware/validation.middleware');
-const{authLimiter} = require('../middleware/rateLimit.middleware')
+const {authLimiter} = require('../middleware/rateLimit.middleware')
 
 router.post('/login',authLimiter,validateLogin,  login);
 router.post('/register',authLimiter, validateRegister,register);
